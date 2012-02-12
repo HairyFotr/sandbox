103173a5fe2dc4836a6187444c3c305fe1a957bc
HairyFotr <hairyfotr@gmail.com>
2012-02-12 15:24:21 +0100

    scalacrash2

--- test/scalacrash.scala
+++ test/scalacrash.scala
@@ -1,5 +1,13 @@
 //crashes 2.9.1 and 2.10.0-M1 compiler
-object scalacrash extends App {
+/*object scalacrash extends App {
 	val reg = "regex".r
 	val reg(a*) = "input"
+}*/
+object scalacrash extends App {
+	class c {
+		def unapplySeq(s:String) = Some(List(s))
+	}
+	val foo = new c
+	val foo(bar*) = "input"
 }
+
